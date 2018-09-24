8a6256ee864c13dcdae5aeacbc4515788574533e
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:34:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1 
+    return array.join
+    elsif array.length == 2 
+    return array.join(" and ") 
+    elsif array.length >= 3
+    newString = "and #{array[-1]}"
+    array.pop
+    array << newString
+    array.join(", ") 
+  end 
+end 
