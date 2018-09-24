7f2eb8dabbb2934c06f9054286eb220af92c3b10
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:34:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,10 @@ def oxford_comma(array)
     return array.join
     elsif array.length == 2 
     return array.join(" and ") 
-    elsif array.length == 3
-    array.join(", ")
-    lastString = array[-1]
-    commaAdd = array.pop
-    return commaAdd << "and" << lastString 
+    elsif array.length >= 3
+    newString = "and #{array[-1]}"
+    array.pop
+    array << newString
+    array.join(", ") 
   end 
-
-end
+end 
