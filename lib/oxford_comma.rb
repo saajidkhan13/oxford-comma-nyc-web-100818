3d73ac299e6307132ace666a23184991e13e6824
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:30:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1 
+    return array.join
+    elsif array.length == 2 
+    return array.join(" and ") 
+    elsif array.length == 3
+    array.join(", ")
+    lastString = array[-1]
+    commaAdd = array.pop
+    return commaAdd << "and" << lastString 
+  end 
 
 end
